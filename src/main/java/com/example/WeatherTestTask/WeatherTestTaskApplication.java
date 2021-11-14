7adbd4e630bfdf0b20a5c72690895f8bce05273f
AndriyGu <andriy.gumich@gmail.com>
2021-11-14 18:48:23 +0300

    Create repo

---
+++ src/main/java/com/example/WeatherTestTask/WeatherTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeatherTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeatherTestTask/WeatherTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeatherTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
